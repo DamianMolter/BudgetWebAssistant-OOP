c4a5dbd9e62f9049469090440990da9329c10704
DamianMolter <163694119+DamianMolter@users.noreply.github.com>
2026-01-12 23:29:00 +0100

    Automted current year display

--- src/App/views/partials/_footer.php
+++ src/App/views/partials/_footer.php
@@ -1,5 +1,5 @@
 <footer class="my-5 pt-5 text-body-secondary text-center text-small">
-      <p class="mb-1">© 2024 By Damian Molter</p>
+      <p class="mb-1">© <?php echo date("Y"); ?> By Damian Molter</p>
 </footer>
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
